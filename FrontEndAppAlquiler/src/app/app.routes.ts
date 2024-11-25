e3940c8472bce95edf89acc74a0dd787c331c606
matias9486 <matias9486mdp@gmail.com>
2024-11-25 18:53:09 -0300

    feat: se agregaron rutas para administrar autos. Y se instalo ngToast para notificaciones y se importaron sus estilos.

--- FrontEndAppAlquiler/src/app/app.routes.ts
+++ FrontEndAppAlquiler/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { ListCarComponent } from './features/admin/cars/list-car/list-car.component';
+import { AddCarComponent } from './features/admin/cars/add-car/add-car.component';
 
 export const routes: Routes = [
     {
@@ -15,6 +17,14 @@ export const routes: Routes = [
     },
     {
         path: 'signup', component: RegisterComponent
-    }
+    },
+
+    {
+        path: 'cars', children: [
+            { path:'', component: ListCarComponent },
+            { path:'add', component: AddCarComponent },
+            { path:'edit/:id', component: AddCarComponent },            
+        ]        
+    },
 ];
 
